33caf81862792453b73dc7b2583eb147c5b5bd04
melany.estevez <elibibialany@gmail.com>
2023-03-06 16:28:11 -0400

    added inicio

--- demo/src/App.js
+++ demo/src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
-import Home from './components/Home';
+import Home from "./components/Home";
+import Layout from "./components/Layout";
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="login" element={<Login/>} />
+        <Route index="/" element={<Layout />} />
+        <Route path="login" element={<Login />} />
         <Route path="signup" element={<Signup />} />
         <Route path="home" element={<Home />} />
       </Routes>
---
+++ demo/src/components/Layout.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Layout() {
+  return (
+    <div>Welcome
+        <br></br>
+        <a href='/login'>Login</a>
+        <br></br>
+        <a href='/signup'>Register</a>
+        <br></br>
+    </div>
+  )
+}
+
+export default Layout
